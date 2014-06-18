078b1794b6ed326e0c3fd5534d69b5075a356460
Caleb Brewer <calebthebrewer@gmail.com>
2014-06-18 02:50:17 -0500

    adding more events to cdbFx, fixing User module to support promises properly

--- src/app.js
+++ src/app.js
@@ -17,52 +17,87 @@
 			});
 			$routeProvider.otherwise({redirectTo: '/'});
 		}])
-		.factory('User', ['$http', '$q', '$cookies', function($http, $q, $cookies) {
-			var user = {},
-				deffered = $q.defer();
+		.factory('User', ['$http', '$q', '$cookieStore', function($http, $q, $cookies) {
+			var user = {};
 
 			return {
-				setUser: function(newUser) {
+				login: function(newUser) {
+					var deferred = $q.defer();
+
 					$http.post('api/user', newUser)
 						.success(function(data) {
 							user = data;
-							$cookies.user = user._id;
+							$cookies.put('user', parseInt(user._id));
+							deferred.resolve(user);
 						})
 						.error(function(data) {
+							$cookies.remove('user');
+							deferred.resolve();
 							throw new Error(data);
 						});
+					return deferred.promise;
+				},
+				logout: function() {
+					var deferred = $q.defer();
+
+					$cookies.remove('user');
+					user = {};
+					deferred.resolve();
+					return deferred.promise;
 				},
 				getUser: function() {
-					if (!user.name && ($cookies.user && $cookies.user != "undefined")) {
-						$http.get('api/user/' + $cookies.user)
+					var deferred = $q.defer();
+
+					if (!user.name && $cookies.get('user')) {
+						$http.get('api/user/' + $cookies.get('user'))
 							.success(function(data) {
-							user = data;
-							deffered.resolve();
-						});
-						return deffered.promise;
+								user = data;
+								deferred.resolve(user);
+							})
+							.error(function() {
+								user = {};
+								$cookies.remove('user');
+								deferred.resolve();
+							});
 					} else {
-						return user;
+						deferred.resolve(user);
 					}
+					return deferred.promise;
 				}
 			};
 		}])
-		.controller('navigation', ['$scope', function($scope) {
-
-		}])
 		.controller('login', ['$scope', '$location', 'User', function($scope, $location, User) {
 
-			$scope.user = User.getUser();
+			User.getUser().then(function(user) {
+				$scope.user = user;
+			});
 
 			$scope.ok = function() {
 
-				User.setUser($scope.user);
-				$location.path('/node');
+				User.login($scope.user).then(function(user) {
+					if (user._id) {
+						$location.path('/node');
+					}
+				});
 			};
 		}])
-		.controller('node', ['$scope', 'User', function($scope, User) {
-			//TODO keep this like this but handle promises too
-			$scope.user = User.getUser();
+		.controller('node', ['$scope', '$location', 'User', function($scope, $location, User) {
+			User.getUser().then(function(user) {
+				if (!user._id) {
+					$location.path('/');
+				}
+				$scope.user = user;
+			});
+
+			$scope.logout = function() {
+				User.logout();
+				$location.path('/');
+
+			};
 		}])
+	/**
+	 * action:parameter,trigger:value[;effect;effect...]
+	 */
 		.directive('cdbFx', [function() {
 
 			return {
@@ -70,21 +105,39 @@
 				scope: false,
 				link: function(scope, element, attributes) {
 
+					//effects
+					function effects(effectParams) {
+						switch (effectParams[0]) {
+							case "removeClass":
+								return function() {
+									element.removeClass(effectParams[1]);
+								}
+							default:
+								console.error("Effect " + effectParams[0] + " is not defined.");
+								break;
+						}
+					};
+
 					element.addClass("animate");
 
 					angular.forEach(attributes.cdbFx.split(";"), function(effect) {
-
 						var options = effect.split(",");
-						setTimeout(function() {
-							switch (options[0]) {
-								case "removeClass":
-									element.removeClass(options[1]);
-									break;
-								default:
-									console.error("Effect " + options[0] + " is not defined.");
-									break;
-							}
-						}, options[2] * 1000);
+
+						var effect = effects(options[0].split(":"))
+
+						//trigger
+						var triggerParams = options[1].split(':') || [null];
+						switch (triggerParams[0]) {
+							case 'after':
+								setTimeout(effect, parseFloat(triggerParams[1]) * 1000);
+								break;
+							case 'on':
+								scope.$on(triggerParams[1], effect)
+								break;
+							default:
+								effect();
+								break;
+						}
 					});
 				}
 			}
